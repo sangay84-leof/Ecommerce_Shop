79b68d207920250450b89a983a9170cd3d67a93e
Sangay <152170146+sangay84-leof@users.noreply.github.com>
2024-09-08 13:17:19 +0530

    Hw
    sdfd

---
+++ conditional.java
@@ -0,0 +1,19 @@
+public class conditional {
+    public static void main(String[] args) {
+        
+        int age = 17;
+
+        if(age > 18){
+            System.out.println("You can drive a war");
+        }
+        else if(age == 2){
+            System.out.println("You are still Child");
+        }
+        else if(age == 17){
+            System.out.println("You are not a adults");
+        }
+        else{
+            System.out.println("You cannot Drive a Car");
+        }
+    }
+}
